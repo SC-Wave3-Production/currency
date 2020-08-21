4095ff5c3e25d627b241ccec849a636818287ba8
DN0p <darya.dunch@gmail.com>
2020-08-22 02:06:09 +0300

    filling dropdowns & button functionality

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 async function getCurrencyList() {
-  console.log('getCurrencyList')
-  const result = await axios('https://api.exchangeratesapi.io/latest')
-  console.log('currencies list are the following', Object.keys(result.data.rates))
-  return Object.keys(result.data.rates)
-}
-getCurrencyList()
-
-async function getCurrencyRates(currency1, currency2, count, date = 'latest') {
-  console.log('getCurrencyRates')
-  const result = await axios(`https://api.exchangeratesapi.io/${date}?base=${currency1}`)
-  console.log('rates are the following', result.data.rates[currency2] * count)
-  return result.data.rates[currency2] * count
+    const result = await axios('https://api.exchangeratesapi.io/latest')
+    console.log('currencies list are the following',Object.keys(result.data.rates))
+    return Object.keys(result.data.rates)
 }
 
-getCurrencyRates('USD', 'EUR', 1)
+
+async function getCurrencyRates(currency1, currency2, count, date='latest') {
+    const result = await axios(`https://api.exchangeratesapi.io/${date}?base=${currency1}`)
+    console.log('rates are the following',result.data.rates[currency2]*count)
+    return result.data.rates[currency2]*count
+}
+
+
+function createOption(selectID, textCurrency) {
+    let newOption = document.createElement('option')
+    document.getElementById(selectID).appendChild(newOption).innerText = textCurrency;
+}
+
+
+getCurrencyList().then(item => item.forEach(it => {
+    createOption('dropdown-1', it)
+    createOption('dropdown-2', it)
+}))
+
+
+document.getElementById('calc-btn').addEventListener('click', function () {
+    const firstCurrency = document.getElementById('dropdown-1').value
+    const secondCurrency = document.getElementById('dropdown-2').value
+    const count = document.getElementById('currency-1').value
+    getCurrencyRates(firstCurrency, secondCurrency, count).then(count => document.getElementById('currency-2').value = count)
+
+})
+
